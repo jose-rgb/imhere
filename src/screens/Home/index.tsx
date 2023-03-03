2e69c21224d04fb50c92d30ed0f3e98c10b60151
jose-rgb <josericardosaraiva7@gmail.com>
2023-03-03 08:55:19 -0300

    add input e button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+
+    function handleParticipantAdd() {
+
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>
@@ -11,6 +16,22 @@ export default function Home() {
             <Text style={styles.eventDate}>
                 sexta, 4 de novembro de 2023.
             </Text>
+
+            <View style={styles.form}>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6B6B6B"
+                    keyboardType="default"
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}> 
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
